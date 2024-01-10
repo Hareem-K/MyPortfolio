d7f654af36b88396f821bd8402d93334ecbc57e5
Hareem Khan <hareem.khan@ucalgary.ca>
2024-01-10 01:17:45 -0700

    updated resume

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -9,7 +9,7 @@ function HeroSection() {
       <h1>Welcome</h1>
       <img className='image' src="https://i.postimg.cc/SNJFPCpS/photo.png" alt="Hareem Khan" />
       <p className='description'>
-        My name is Hareem Khan, and I am a motivated third-year Software Engineering student at the University of Calgary. I am passionate about software development and actively seeking internship opportunities to enhance my knowledge and skills. With a strong foundation in programming, I am experienced in Java, Python, C, C++, SQL, JavaScript (Vanilla JS and React.js), CSS, and HTML.
+        My name is Hareem Khan, and I am a motivated third-year Software Engineering student at University of Calgary. I am passionate about software development and actively seeking internship opportunities to enhance my knowledge and skills. With a strong foundation in programming, I am experienced in Java, Python, C, C++, SQL, JavaScript (Vanilla JS and React.js), CSS, and HTML.
       </p>
       <div className='hero-btns'>
         {/* Add your buttons or other content here */}
