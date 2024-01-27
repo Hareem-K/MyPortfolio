dac52d7098d3c49f492d93a6491d458abc23105f
Hareem Khan <hareem.khan@ucalgary.ca>
2024-01-27 00:10:46 -0700

    update resume

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -6,12 +6,12 @@ import Footer from '../Footer';
 import { Button } from '../Button';
 
 export default function Resume() {
-  const downloadLink = 'https://drive.google.com/file/d/1ixhcyEU9Gox2NyGiQ4Blmn77Le0vEerY/view?usp=sharing';
+  const downloadLink = 'https://drive.google.com/file/d/15toBhMA7hIGz6GfedExhvNmzSjvozp6G/view?usp=sharing';
 
   return (
     <>
       <h1 className='resume'>RESUME</h1>
-      <img className='resumeImage' src="images/resume.png" alt="Resume" />
+      <img className='resumeImage' src="images/HareemResume.png" alt="Resume" />
 
       <div style={{ display: 'flex', justifyContent: 'center', paddingBottom: '50px', paddingTop: '30px', maxWidth: '197px', margin: '0 auto' }}>
         <Button className='btns' buttonStyle='btn--download' buttonSize='btn--large' linkTo={downloadLink} external>
