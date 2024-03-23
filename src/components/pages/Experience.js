0fc754b204b15f32661535af563d83a0a3d70392
Hareem Khan <hareem.khan@ucalgary.ca>
2024-03-23 14:58:58 -0600

    added credential link

--- src/components/pages/Experience.js
+++ src/components/pages/Experience.js
@@ -161,8 +161,8 @@ export default function Experience() {
                         </ul>
                     </div>
                     <div style={{ display: 'flex', justifyContent: 'center', maxWidth: '217px', margin: '0 auto', marginTop: '10px' }}>
-                        <Button className='btns' buttonStyle='btn--download' buttonSize='btn--large' linkTo={"https://www.ibm.com/training/badge/web-development-fundamentals"} external>
-                        Credential in Progress <i className='fas fa-arrow-right' />
+                        <Button className='btns' buttonStyle='btn--download' buttonSize='btn--large' linkTo={"https://www.credly.com/badges/6a0adff7-21be-4060-8802-bc397cfef050/public_url"} external>
+                        View Credential <i className='fas fa-arrow-right' />
                         </Button>
                     </div>
                 </div>
