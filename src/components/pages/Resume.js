132cba8113aa9c1c8bff6101bc77932a0abafbde
Hareem Khan <hareem.khan@ucalgary.ca>
2024-09-04 20:45:20 -0600

    added projects and updated resume

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -11,12 +11,12 @@ export default function Resume() {
     window.scrollTo(0, 0);
   }, []);
 
-  const downloadLink = 'https://drive.google.com/file/d/1Mi_CNTMhcLJrsaKwRGI-J5_Wv00uR3a7/view?usp=sharing';
+  const downloadLink = 'https://drive.google.com/file/d/1RTFkx5pG3rKWK7wd4ZqK0q-x1pCOUR9L/view?usp=sharing';
   
   return (
     <>
       <h1 className='resume'>RESUME</h1>
-      <iframe className='resumePreview' src="https://drive.google.com/file/d/1Mi_CNTMhcLJrsaKwRGI-J5_Wv00uR3a7/preview" title="My Resume" allow="autoplay"></iframe>
+      <iframe className='resumePreview' src="https://drive.google.com/file/d/1RTFkx5pG3rKWK7wd4ZqK0q-x1pCOUR9L/preview" title="My Resume" allow="autoplay"></iframe>
       <div style={{ display: 'flex', justifyContent: 'center', paddingBottom: '50px', paddingTop: '30px', maxWidth: '197px', margin: '0 auto' }}>
         <Button className='btns' buttonStyle='btn--download' buttonSize='btn--large' linkTo={downloadLink} external>
           DOWNLOAD <i className='fas fa-download' />
